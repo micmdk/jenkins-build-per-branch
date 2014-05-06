948b1de2f346665e50a6b83870c6a861d52beb3a
Mortensen, Michael <mmortensen@ebay.com>
2014-05-06 13:28:13 +0200

    check for needed templates for names with branch name at any position

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -98,7 +98,7 @@ class JenkinsJobManager {
     }
 
     List<TemplateJob> findRequiredTemplateJobs(List<String> allJobNames) {
-        String regex = /^(?!Master($templateJobPrefix))-($templateBranchName)-[^-]*$/
+        String regex = /^($templateJobPrefix)-($templateBranchName)-[^-]*$/
 
         List<TemplateJob> templateJobs = allJobNames.findResults { String jobName ->
             TemplateJob templateJob = null
